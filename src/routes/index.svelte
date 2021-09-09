dafec3412bb0a5e39a45fb9f50c8957ca4253d1f
Xavier Haniquaut <xavier.haniquaut@decathlon.com>
2021-09-09 19:44:20 +0200

    start adding content

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,59 +1,70 @@
 <script context="module" lang="ts">
+  import { browser, dev } from '$app/env';
+
+  // we don't need any JS on this page, though we'll load
+  // it in dev so that we get hot module replacement...
+  export const hydrate = dev;
+
+  // ...but if the client-side router is already loaded
+  // (i.e. we came here from elsewhere in the app), use it
+  export const router = browser;
+
+  // since there's no dynamic data here, we can prerender
+  // it so that it gets served as a static asset in prod
   export const prerender = true;
 </script>
 
 <script lang="ts">
-  import Counter from '$lib/Counter.svelte';
 </script>
 
 <svelte:head>
-  <title>Home</title>
+  <title>Alices | Lille</title>
 </svelte:head>
 
-<section>
-  <h1>
-    <div class="welcome">
-      <picture>
-        <source srcset="svelte-welcome.webp" type="image/webp" />
-        <img src="svelte-welcome.png" alt="Welcome" />
-      </picture>
+<div class="wrapper">
+  <div class="logo">
+    Alices
+  </div>
+
+  <div class="cbsc">
+    Café • Bar • Saisonnier • Culturel
+  </div>
+
+  <div class="buttons">
+    <div class="menu">
+      <button>
+        Découvrir la carte
+      </button>
     </div>
 
-    to your new<br />SvelteKit app
-  </h1>
+    <div class="map">
+      <button>
+        Nous rendre visite
+      </button>
+    </div>
+  </div>
 
-  <h2>
-    try editing <strong>src/routes/index.svelte</strong>
-  </h2>
+  <hr/>
 
-  <Counter />
-</section>
+  <div class="open_title">
+    Horaires d'ouverture
+  </div>
 
-<style>
-  section {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    flex: 1;
-  }
+  <hr/>
 
-  h1 {
-    width: 100%;
-  }
+  <div class="open_hours">
+    Mardi au Jeudi: 15h - 00H
+    <br/>
+    Vendredi: 15h - 1h
+    <br/>
+    Samedi: 15h - 2h
+  </div>
 
-  .welcome {
-    position: relative;
-    width: 100%;
-    height: 0;
-    padding: 0 0 calc(100% * 495 / 2048) 0;
-  }
+  <hr/>
 
-  .welcome img {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    display: block;
-  }
-</style>
+  <div class="social_networks">
+    Facebook - Insta - Mapster
+  </div>
+
+  <hr/>
+</div>
