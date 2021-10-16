2725ef2f3661d39c5ab4059d111a501eef074495
Xavier Haniquaut <xavier.haniquaut@decathlon.com>
2021-10-16 19:13:30 +0200

    start building on netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import adapter from '@sveltejs/adapter-netlify';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,6 +9,7 @@ const config = {
 
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
+    adapter: adapter(),
     target: '#svelte',
   },
 };
