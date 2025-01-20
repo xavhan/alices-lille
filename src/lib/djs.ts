9e0754fb4a9b99a906b79939fcca5536789a40cc
Xavier Haniquaut <xavier.haniquaut@gmail.com>
2025-01-20 19:13:25 +0100

    bootstrap

--- src/lib/djs.ts
+++
@@ -1,4 +0,0 @@
-import { DJS } from '$data/djs';
-import type { DJ } from '$data/types';
-
-export const getDJ = (slug: DJ['slug']) => DJS.find((e) => e.slug === slug);
--- src/lib/list.ts
+++
@@ -1,5 +0,0 @@
-const formatter = new Intl.ListFormat('fr-FR', {
-  style: 'short',
-  type: 'conjunction',
-});
-export const list = (items: string[]) => formatter.format(items);
