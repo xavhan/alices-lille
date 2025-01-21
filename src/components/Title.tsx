285e521b094500faae107f348f25284d4d2f2e7d
Xavier Haniquaut <xavier.haniquaut@gmail.com>
2025-01-21 15:19:16 +0100

    homepage

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,7 +1,13 @@
-type TitleProps = {
+import { ComponentPropsWithRef } from "react";
+
+type TitleProps = ComponentPropsWithRef<"h2"> & {
   children: React.ReactNode;
 };
 
-export const Title = ({ children }: TitleProps) => (
-  <h2 className="">{children}</h2>
+export const Title = ({ className, children, ...rest }: TitleProps) => (
+  <h2 className={className + " w-full"} {...rest}>
+    {children}
+  </h2>
 );
+
+// todo: use cn
